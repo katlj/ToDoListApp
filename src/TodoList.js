12d23cc8c4ea0ebf927945107d2073e3447256d0
katlj <kathurnalj@gmail.com>
2024-04-06 11:35:20 -0400

    added todolist componenet, figuring out functionality.

---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {useState} from "react";
+import Card from "react-bootstrap/Card";
+
+function TodoList(){
+    const[task, setTasks] = useState("Click here to edit text")
+        //states are anything that can be updated. I want the card to be able to be updated
+
+   
+}
+ export default TodoList;
