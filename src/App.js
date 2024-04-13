8d9d098e40b42208fc4d02c6613ac9cd55800a73
katlj <kathurnalj@gmail.com>
2024-04-12 20:49:36 -0400

    first page done. added button and gradient

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import './App.css';
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
     <div>
     <div className="app_header">DOOLY</div>
     <div className="subheader">the only to do list app you need</div>
-    
+    <button className="add_item "><img src="/Images/plussign.png" alt="plus"></img></button>
     </div>
   );
 }
